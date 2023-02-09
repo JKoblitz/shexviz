ba8926e2db22ef0c71ffd719f672355df0ce3d82
Andra Waagmeester <andra@micelio.be>
2023-02-09 20:51:03 +0100

    test package release

--- src/shexviz/shex2dot.py
+++ src/shexviz/shex2dot.py
@@ -3,63 +3,6 @@ from ShExJSG.ShExJ import Shape, IRIREF, TripleConstraint, NodeConstraint, Shape
 from pyshex.utils.schema_loader import SchemaLoader
 import graphviz
 
-def process_tc(tc, shape_id):
-    print(tc)
-    dotschema.node(startshape.replace(":", ""), startshape, shape=symbol["iri"])
-    if isinstance(tc, TripleConstraint):
-        if isinstance(tc.valueExpr, IRIREF):
-            node = tc.valueExpr
-            predicate = tc.predicate
-            for key in prefixmap.keys():
-                node = node.replace(key, prefixmap[key])
-                predicate = predicate.replace(key, prefixmap[key] + ":")
-            dotschema.node(node, node, shape=symbol["iri"])
-            dotschema.edge(shape.id.split("/")[-1], node, label=predicate)
-        elif isinstance(tc.valueExpr, NodeConstraint):
-
-            if tc.valueExpr.datatype:
-                datatype = tc.valueExpr.datatype
-                predicate = tc.predicate
-                for key in prefixmap.keys():
-                    datatype = datatype.replace(key, prefixmap[key] + ":")
-                    predicate = predicate.replace(key, prefixmap[key] + ":")
-                dotschema.node(
-                    shape.id.split("/")[-1] + tc.valueExpr.datatype.split("/")[-1] + tc.predicate.split("/")[-1],
-                    datatype, shape=symbol["datatype"])
-                dotschema.edge(shape.id.split("/")[-1],
-                               shape.id.split("/")[-1] + tc.valueExpr.datatype.split("/")[-1] + tc.predicate.split("/")[
-                                   -1], label=predicate)
-            elif tc.valueExpr.values:
-                oneofs = []
-                predicate = tc.predicate
-                for value in tc.valueExpr.values:
-                    for key in prefixmap.keys():
-                        value = value.replace(key, prefixmap[key] + ":")
-                    oneofs.append(value)
-                for key in prefixmap.keys():
-                    predicate = predicate.replace(key, prefixmap[key] + ":")
-                dotschema.node(
-                    shape.id.split("/")[-1] + "|".join(oneofs).replace(":", "") + tc.predicate.split("/")[-1],
-                    "{" + "|".join(oneofs) + "}", shape=symbol["oneof"])
-                dotschema.edge(shape.id.split("/")[-1],
-                               shape.id.split("/")[-1] + "|".join(oneofs).replace(":", "") + tc.predicate.split("/")[
-                                   -1], label=predicate)
-
-            elif tc.valueExpr.nodeKind:
-                dotschema.node(tc.valueExpr.nodeKind, tc.valueExpr.nodeKind.split("/")[-1],
-                               shape=symbol[tc.valueExpr.nodeKind])
-                dotschema.edge(shape.id.split("/")[-1], tc.valueExpr.nodeKind.split("/")[-1],
-                               label=tc.predicate.split("/")[-1])
-            elif tc.valueExpr.xone:
-
-                dotschema.node(tc.valueExpr.xone[0].id, tc.valueExpr.xone[0].id.split("/")[-1], shape=symbol["oneof"])
-                dotschema.edge(shape.id.split("/")[-1], tc.valueExpr.xone[0].id.split("/")[-1],
-                               label=tc.predicate.split("/")[-1])
-            else:
-                print("No valueExpr")
-        else:
-            print("No valueExpr")
-
 symbol = dict()
 symbol["class"] = "oval"
 symbol["datatype"] = "octagon"
@@ -67,9 +10,6 @@ symbol["literal"] = "rectangle"
 symbol["iri"]="diamond"
 symbol["bnode"]='point'
 symbol["oneof"]='record'
-
-dotschema = graphviz.Digraph('shape.gv')
-dotschema.graph_attr['rankdir'] = 'LR'
 
 shex = """
 IMPORT <file_metadata.shex>
@@ -118,36 +58,104 @@ start = @:subject
     qudt:numericValue xsd:decimal ;
 }
   """
-prefixmap = dict()
-for line in shex.splitlines():
-    if line.startswith("PREFIX"):
-        line = line.replace("PREFIX", "")
-        print(line)
-        prefix, uri = line.split(": ")
-        prefix = prefix.strip()
-        uri = uri.strip()
-        prefixmap[uri.replace("<", "").replace(">", "")] = prefix
-        dotschema.node(prefix, uri, shape="none", style="invis")
 
-loader = SchemaLoader()
-schema = loader.loads(shex)
-# print(ShExJ(schema))
-print(f"Valid: {schema._is_valid()}")
-#print(f"{schema._as_json_dumps()}")
-print(schema.shapes[0].expression.expressions)
-for shape in schema.shapes:
-    print(shape.id)
-    startshape = shape.id
-    for key in prefixmap.keys():
-        startshape = startshape.replace(key, prefixmap[key]+":")
-    if "expressions" in dir(shape.expression):
-        for tc in shape.expression.expressions:
+def shex2dot(shex, graphviz_name, format="png", rankdir="LR"):
+    def process_tc(tc, shape_id):
+        print(tc)
+        dotschema.node(startshape.replace(":", ""), startshape, shape=symbol["iri"])
+        if isinstance(tc, TripleConstraint):
+            if isinstance(tc.valueExpr, IRIREF):
+                node = tc.valueExpr
+                predicate = tc.predicate
+                for key in prefixmap.keys():
+                    node = node.replace(key, prefixmap[key])
+                    predicate = predicate.replace(key, prefixmap[key] + ":")
+                dotschema.node(node, node, shape=symbol["iri"])
+                dotschema.edge(shape.id.split("/")[-1], node, label=predicate)
+            elif isinstance(tc.valueExpr, NodeConstraint):
+
+                if tc.valueExpr.datatype:
+                    datatype = tc.valueExpr.datatype
+                    predicate = tc.predicate
+                    for key in prefixmap.keys():
+                        datatype = datatype.replace(key, prefixmap[key] + ":")
+                        predicate = predicate.replace(key, prefixmap[key] + ":")
+                    dotschema.node(
+                        shape.id.split("/")[-1] + tc.valueExpr.datatype.split("/")[-1] + tc.predicate.split("/")[-1],
+                        datatype, shape=symbol["datatype"])
+                    dotschema.edge(shape.id.split("/")[-1],
+                                   shape.id.split("/")[-1] + tc.valueExpr.datatype.split("/")[-1] +
+                                   tc.predicate.split("/")[
+                                       -1], label=predicate)
+                elif tc.valueExpr.values:
+                    oneofs = []
+                    predicate = tc.predicate
+                    for value in tc.valueExpr.values:
+                        for key in prefixmap.keys():
+                            value = value.replace(key, prefixmap[key] + ":")
+                        oneofs.append(value)
+                    for key in prefixmap.keys():
+                        predicate = predicate.replace(key, prefixmap[key] + ":")
+                    dotschema.node(
+                        shape.id.split("/")[-1] + "|".join(oneofs).replace(":", "") + tc.predicate.split("/")[-1],
+                        "{" + "|".join(oneofs) + "}", shape=symbol["oneof"])
+                    dotschema.edge(shape.id.split("/")[-1],
+                                   shape.id.split("/")[-1] + "|".join(oneofs).replace(":", "") +
+                                   tc.predicate.split("/")[
+                                       -1], label=predicate)
+
+                elif tc.valueExpr.nodeKind:
+                    dotschema.node(tc.valueExpr.nodeKind, tc.valueExpr.nodeKind.split("/")[-1],
+                                   shape=symbol[tc.valueExpr.nodeKind])
+                    dotschema.edge(shape.id.split("/")[-1], tc.valueExpr.nodeKind.split("/")[-1],
+                                   label=tc.predicate.split("/")[-1])
+                elif tc.valueExpr.xone:
+
+                    dotschema.node(tc.valueExpr.xone[0].id, tc.valueExpr.xone[0].id.split("/")[-1],
+                                   shape=symbol["oneof"])
+                    dotschema.edge(shape.id.split("/")[-1], tc.valueExpr.xone[0].id.split("/")[-1],
+                                   label=tc.predicate.split("/")[-1])
+                else:
+                    print("No valueExpr")
+            else:
+                print("No valueExpr")
+    dotschema = graphviz.Digraph(graphviz_name, format="png")
+    dotschema.attr(rankdir=rankdir)
+    prefixmap = dict()
+    for line in shex.splitlines():
+        if line.startswith("PREFIX"):
+            line = line.replace("PREFIX", "")
+            print(line)
+            prefix, uri = line.split(": ")
+            prefix = prefix.strip()
+            uri = uri.strip()
+            prefixmap[uri.replace("<", "").replace(">", "")] = prefix
+            dotschema.node(prefix, uri, shape="none", style="invis")
+
+    loader = SchemaLoader()
+    schema = loader.loads(shex)
+    print(f"Valid: {schema._is_valid()}")
+    print(schema.shapes[0].expression.expressions)
+    for shape in schema.shapes:
+        print(shape.id)
+        startshape = shape.id
+        for key in prefixmap.keys():
+            startshape = startshape.replace(key, prefixmap[key]+":")
+        if "expressions" in dir(shape.expression):
+            for tc in shape.expression.expressions:
+                process_tc(tc, startshape)
+        else:
+            tc = shape.expression
             process_tc(tc, startshape)
-    else:
-        tc = shape.expression
-        process_tc(tc, startshape)
 
-dotschema.view()
+    return dotschema
 
-print(prefixmap)
-print(dotschema.source)
+def view_graphviz(dotschema):
+    return dotschema.view()
+
+def save_graphviz(dotschema, filename):
+    return dotschema.render(filename)
+
+
+
+
